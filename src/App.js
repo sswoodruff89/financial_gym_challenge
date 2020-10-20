8ff6a6587ee603c234ae4e5dcc0afa370e27cbad
sean <sswoodruff89@gmail.com>
2020-10-20 19:47:59 -0400

    got rate list from exchange_rate_api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {getRates} from './util/exchange_rate_api';
 import './App.css';
 
 const App = () => {
+  let rates = useRef();
+  const [listLoaded, setListLoaded] = useState(false);
+  const [rateList, setRateList] = useState(null);
+  const [base, setBase] = useState("EUR");
 
   useEffect(() => {
-    getRates()
+    if (!rateList) {
+      
+    //   setListLoaded(true);
+
+    // } else {
+      
+      getRates()
       .then((res) => {
-        console.log(res);
+        // rates.current = res.data.rates;
+        // localStorage.setItem("rates", res.data.rates);
+        // setListLoaded(true);
+        //       console.log(rates.current)
+        setRateList(res.data.rates);
+        // console.log(res.data.rates);
       });
-    
+    // }
+    }
   }, []);
 
+  console.log(rateList);
   return (
     <main className="App">
+      <table className="rateList">
+        <tbody>
+          <tr className="rate-header">
+            <td>Currency</td>
+            <td>Rate</td>
+          </tr>
+        {rateList && Object.keys(rateList).map((rate, i) => (
+          <tr key={i} className="rate-item">
+            <td>{rate}</td>
+            <td>{rateList[rate]}</td>
+          </tr>
+        ))}
+          
+        </tbody>
+      </table>
 
-    Hello World
+
     </main>
   );
 }
